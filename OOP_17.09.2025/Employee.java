f1e1227ebc3195223c5c3fd64db0c47f413bf4ab
sivachelvan2002 <s.sivachelvan2002@gmail.com>
2025-09-17 14:10:06 +0530

    Add Employee class and main program
    Introduced Employee.java with fields for id, name, salary, and bonus calculation logic. Added MainEmployee.java to demonstrate creating an Employee, setting details, calculating bonus, and displaying information.

---
+++ OOP_17.09.2025/Employee.java
@@ -0,0 +1,32 @@
+class Employee{
+	int id;
+	String name;
+	double basic_salary;
+	double bonus=0.0;
+	double total_salary=0.0;
+	
+	void setEmployee(int ids,String names,double basic_salarys){
+		id=ids;
+		name=names;
+		basic_salary=basic_salarys;
+	}
+	
+	void bonusCalculation(){
+		if(basic_salary>=100000){
+			bonus=(basic_salary*0.1);
+		}
+		else{
+			bonus=(basic_salary*0.05);
+		}
+		total_salary=basic_salary+bonus;
+	}
+	
+	void Display(){
+		System.out.println("Id : "+id);
+		System.out.println("Name : "+name);
+		System.out.println("Basic Salary : "+basic_salary);
+		System.out.println("Total Salary : "+total_salary);
+	}
+}
+		
+			
---
+++ OOP_17.09.2025/MainEmployee.java
@@ -0,0 +1,9 @@
+class MainEmployee{
+	public static void main(String args[]){
+		Employee emp=new Employee();
+		
+		emp.setEmployee(1000,"selvan",56000);
+		emp.bonusCalculation();
+		emp.Display();
+	}
+}
