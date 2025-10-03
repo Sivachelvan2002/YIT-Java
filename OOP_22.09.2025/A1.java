f50ece4e32bbc508959fb931908b3f7994819eb1
sivachelvan2002 <s.sivachelvan2002@gmail.com>
2025-10-03 15:25:26 +0530

    Add classes demonstrating inheritance in Java
    Added A1 and B1 classes to illustrate basic inheritance, with B1 extending A1 and overriding the getx() method. Also added MainAB1 to demonstrate object creation and method calls.

---
+++ OOP_22.09.2025/A1.java
@@ -0,0 +1,9 @@
+class A1{
+	int x;
+	A1(){
+		this.x=20;
+	}
+	void getx(){
+		System.out.println("value of x is "+this.x);
+	}
+}
---
+++ OOP_22.09.2025/B1.java
@@ -0,0 +1,12 @@
+class B1 extends A1{
+	int y;
+	B1(){
+		this.y=30;
+	}
+	void gety(){
+		System.out.println("value of y is "+this.y);
+	}
+	void getx(){
+		System.out.println("value of bx is "+this.x);
+	}
+}
